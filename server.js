0a7a49b220c10d0115258c02b576bbf06bef27bc
Cptn Reef <sharif.staffordbz@gmail.com>
2021-02-14 13:45:06 -0500

    3rd commit

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-"use strict";
-
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser')
+const expressValidator = require('express-validator');
 const app = express();
+const exphbs  = require('express-handlebars');
 const port = 3000
 
- 
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes =======================================================================
+require('./controllers/posts.js')(app);
+
+// Database Setup ===============================================================
+require('./data/reddit-db.js');
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
- 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
+
+app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/posts/new', (req, res) => {
+    return res.render('posts/new/posts-new');
 });
 
 app.listen(port, function () {
