54511335d83aae8258af2a839106668045f48f76
Cptn Reef <sharif.staffordbz@gmail.com>
2021-02-17 21:03:23 -0500

    5th commit

--- controllers/posts.js
+++ controllers/posts.js
@@ -21,6 +21,17 @@ module.exports = (app) => {
           .catch(err => {
             console.log(err.message);
           })
-    });    
+    });
+
+    app.get("/posts/:id", function(req, res) {
+        // LOOK UP THE POST
+        Post.findById(req.params.id).lean()
+          .then(post => {
+            res.render("posts-show", { post });
+          })
+          .catch(err => {
+            console.log(err.message);
+          });
+      });
   
 };
